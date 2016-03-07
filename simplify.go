1744cd6fab26aee74df0d589a64c5e0bcc35d3ac
Richard Musiol <mail@richard-musiol.de>
2016-03-07 01:56:08 +0100

    improved Simplify a lot, 100% test coverage now

--- simplify.go
+++ simplify.go
@@ -4,33 +4,324 @@ import (
 	"fmt"
 	"go/ast"
 	"go/token"
+	"go/types"
 )
 
-func Simplify(stmts []ast.Stmt) []ast.Stmt {
-	c := &simplifyContext{}
+type simplifyContext struct {
+	info       *types.Info
+	varCounter int
+}
+
+func Simplify(stmts []ast.Stmt, info *types.Info) []ast.Stmt {
+	c := &simplifyContext{info: info}
 	return c.simplifyStmtList(stmts)
 }
 
 func (c *simplifyContext) simplifyStmtList(stmts []ast.Stmt) []ast.Stmt {
 	var newStmts []ast.Stmt
 	for _, s := range stmts {
-		switch s := s.(type) {
-		case *ast.ExprStmt:
-			newStmts = append(newStmts, &ast.ExprStmt{
-				X: c.simplifyExpr(&newStmts, s.X),
-			})
-		default:
-			newStmts = append(newStmts, s)
-		}
+		c.simplifyStmt(&newStmts, s)
 	}
 	return newStmts
 }
 
-type simplifyContext struct {
-	varCounter int
+func (c *simplifyContext) simplifyStmt(stmts *[]ast.Stmt, s ast.Stmt) {
+	if s == nil {
+		return
+	}
+
+	switch s := s.(type) {
+	case *ast.ExprStmt:
+		*stmts = append(*stmts, &ast.ExprStmt{
+			X: c.simplifyExpr2(stmts, s.X, true),
+		})
+
+	case *ast.BlockStmt:
+		*stmts = append(*stmts, c.simplifyBlock(s))
+
+	case *ast.IfStmt:
+		if s.Init != nil {
+			block := &ast.BlockStmt{}
+			*stmts = append(*stmts, block)
+			stmts = &block.List
+			c.simplifyStmt(stmts, s.Init)
+		}
+		*stmts = append(*stmts, &ast.IfStmt{
+			If:   s.If,
+			Cond: c.simplifyExpr(stmts, s.Cond),
+			Body: c.simplifyBlock(s.Body),
+			Else: toSingleStmt(c.simplifyToStmtList(s.Else)),
+		})
+
+	case *ast.SwitchStmt:
+		c.simplifySwitch(stmts, s)
+
+	case *ast.TypeSwitchStmt:
+		if s.Init != nil {
+			block := &ast.BlockStmt{}
+			*stmts = append(*stmts, block)
+			stmts = &block.List
+			c.simplifyStmt(stmts, s.Init)
+		}
+		var assign ast.Stmt
+		switch a := s.Assign.(type) {
+		case *ast.ExprStmt:
+			ta := a.X.(*ast.TypeAssertExpr)
+			assign = &ast.ExprStmt{
+				X: &ast.TypeAssertExpr{
+					X:      c.simplifyExpr(stmts, ta.X),
+					Lparen: ta.Lparen,
+					Type:   ta.Type,
+					Rparen: ta.Rparen,
+				},
+			}
+		case *ast.AssignStmt:
+			ta := a.Rhs[0].(*ast.TypeAssertExpr)
+			assign = &ast.AssignStmt{
+				Lhs:    a.Lhs,
+				Tok:    a.Tok,
+				TokPos: a.TokPos,
+				Rhs: []ast.Expr{
+					&ast.TypeAssertExpr{
+						X:      c.simplifyExpr(stmts, ta.X),
+						Lparen: ta.Lparen,
+						Type:   ta.Type,
+						Rparen: ta.Rparen,
+					},
+				},
+			}
+		}
+		simplifiedClauses := c.simplifyCaseClauses(s.Body.List)
+		clauses := make([]ast.Stmt, len(simplifiedClauses))
+		for i, entry := range simplifiedClauses {
+			cc := entry.(*ast.CaseClause)
+			clauses[i] = &ast.CaseClause{
+				Case:  cc.Case,
+				List:  cc.List,
+				Colon: cc.Colon,
+				Body:  c.simplifyStmtList(cc.Body),
+			}
+		}
+		*stmts = append(*stmts, &ast.TypeSwitchStmt{
+			Switch: s.Switch,
+			Assign: assign,
+			Body: &ast.BlockStmt{
+				List: clauses,
+			},
+		})
+
+	case *ast.ForStmt:
+		*stmts = append(*stmts, &ast.ForStmt{
+			For:  s.For,
+			Init: s.Init,
+			Cond: s.Cond,
+			Post: s.Post,
+			Body: c.simplifyBlock(s.Body),
+		})
+
+	// case *ast.ForStmt:
+	// 	c.simplifyStmt(stmts, s.Init)
+	// 	var condStmts []ast.Stmt
+	// 	cond := c.newVar(&condStmts, s.Cond)
+	// 	bodyStmts := s.Body.List
+	// 	if len(condStmts) != 0 {
+	// 		bodyStmts = append(append(condStmts, &ast.IfStmt{
+	// 			Cond: &ast.UnaryExpr{
+	// 				Op: token.NOT,
+	// 				X:  cond,
+	// 			},
+	// 			Body: &ast.BlockStmt{
+	// 				List: []ast.Stmt{&ast.BranchStmt{
+	// 					Tok: token.BREAK,
+	// 				}},
+	// 			},
+	// 		}), bodyStmts...)
+	// 		cond = nil
+	// 	}
+	// 	*stmts = append(*stmts, &ast.ForStmt{
+	// 		For:  s.For,
+	// 		Cond: cond,
+	// 		Post: s.Post,
+	// 		Body: &ast.BlockStmt{
+	// 			List: bodyStmts,
+	// 		},
+	// 	})
+
+	case *ast.IncDecStmt:
+		*stmts = append(*stmts, &ast.IncDecStmt{
+			X:      c.simplifyExpr(stmts, s.X),
+			TokPos: s.TokPos,
+			Tok:    s.Tok,
+		})
+
+	case *ast.GoStmt:
+		*stmts = append(*stmts, &ast.GoStmt{
+			Go:   s.Go,
+			Call: c.simplifyCall(stmts, s.Call),
+		})
+
+	case *ast.SelectStmt:
+		clauses := make([]ast.Stmt, len(s.Body.List))
+		for i, entry := range s.Body.List {
+			cc := entry.(*ast.CommClause)
+			clauses[i] = &ast.CommClause{
+				Case:  cc.Case,
+				Comm:  cc.Comm, // FIXME
+				Colon: cc.Colon,
+				Body:  c.simplifyStmtList(cc.Body),
+			}
+		}
+		*stmts = append(*stmts, &ast.SelectStmt{
+			Select: s.Select,
+			Body: &ast.BlockStmt{
+				List: clauses,
+			},
+		})
+
+	case *ast.DeferStmt:
+		*stmts = append(*stmts, &ast.DeferStmt{
+			Defer: s.Defer,
+			Call:  c.simplifyCall(stmts, s.Call),
+		})
+
+	case *ast.SendStmt:
+		*stmts = append(*stmts, &ast.SendStmt{
+			Chan:  c.simplifyExpr(stmts, s.Chan),
+			Arrow: s.Arrow,
+			Value: c.simplifyExpr(stmts, s.Value),
+		})
+
+	default:
+		*stmts = append(*stmts, s)
+	}
+}
+
+func (c *simplifyContext) simplifyBlock(s *ast.BlockStmt) *ast.BlockStmt {
+	return &ast.BlockStmt{
+		Lbrace: s.Lbrace,
+		List:   c.simplifyStmtList(s.List),
+		Rbrace: s.Rbrace,
+	}
+}
+
+func (c *simplifyContext) simplifySwitch(stmts *[]ast.Stmt, s *ast.SwitchStmt) {
+	clauses := c.simplifyCaseClauses(s.Body.List)
+
+	wrapClause := &ast.CaseClause{}
+	*stmts = append(*stmts, &ast.SwitchStmt{
+		Switch: s.Switch,
+		Body:   &ast.BlockStmt{List: []ast.Stmt{wrapClause}},
+	})
+	stmts = &wrapClause.Body
+
+	c.simplifyStmt(stmts, s.Init)
+
+	var tag ast.Expr = ast.NewIdent("true")
+	if s.Tag != nil {
+		tag = c.newVar(stmts, s.Tag)
+	}
+	*stmts = append(*stmts, c.switchToIfElse(tag, clauses)...)
+}
+
+func (c *simplifyContext) simplifyCaseClauses(clauses []ast.Stmt) []ast.Stmt {
+	var newClauses []ast.Stmt
+	var openClauses []*ast.CaseClause
+	var defaultClause *ast.CaseClause
+	for _, cc := range clauses {
+		clause := cc.(*ast.CaseClause)
+		newClause := &ast.CaseClause{
+			Case:  clause.Case,
+			List:  clause.List,
+			Colon: clause.Colon,
+		}
+
+		body := clause.Body
+		hasFallthrough := false
+		if len(body) != 0 {
+			if b, isBranchStmt := body[len(body)-1].(*ast.BranchStmt); isBranchStmt && b.Tok == token.FALLTHROUGH {
+				body = body[:len(body)-1]
+				hasFallthrough = true
+			}
+		}
+		openClauses = append(openClauses, newClause)
+		for _, openClause := range openClauses {
+			openClause.Body = append(openClause.Body, body...)
+		}
+		if !hasFallthrough {
+			openClauses = nil
+		}
+
+		if len(clause.List) == 0 {
+			defaultClause = newClause
+			continue
+		}
+		newClauses = append(newClauses, newClause)
+	}
+
+	if defaultClause != nil {
+		newClauses = append(newClauses, defaultClause)
+	}
+
+	return newClauses
+}
+
+func (c *simplifyContext) switchToIfElse(tag ast.Expr, clauses []ast.Stmt) (stmts []ast.Stmt) {
+	if len(clauses) == 0 {
+		return nil
+	}
+
+	clause := clauses[0].(*ast.CaseClause)
+	if len(clause.List) == 0 {
+		return c.simplifyStmtList(clause.Body)
+	}
+
+	conds := make([]ast.Expr, len(clause.List))
+	for i, cond := range clause.List {
+		conds[i] = &ast.BinaryExpr{X: tag, Op: token.EQL, Y: &ast.ParenExpr{X: cond}}
+	}
+	stmts = append(stmts, &ast.IfStmt{
+		If:   clause.Case,
+		Cond: c.simplifyExpr(&stmts, disjunction(conds)),
+		Body: &ast.BlockStmt{List: c.simplifyStmtList(clause.Body)},
+		Else: toSingleStmt(c.switchToIfElse(tag, clauses[1:])),
+	})
+	return
+}
+
+func disjunction(conds []ast.Expr) ast.Expr {
+	if len(conds) == 1 {
+		return conds[0]
+	}
+	return &ast.BinaryExpr{
+		X:  conds[0],
+		Op: token.LOR,
+		Y:  disjunction(conds[1:]),
+	}
+}
+
+func (c *simplifyContext) simplifyToStmtList(s ast.Stmt) (stmts []ast.Stmt) {
+	c.simplifyStmt(&stmts, s)
+	return
+}
+
+func toSingleStmt(stmts []ast.Stmt) ast.Stmt {
+	switch len(stmts) {
+	case 0:
+		return nil
+	case 1:
+		return stmts[0]
+	default:
+		return &ast.BlockStmt{
+			List: stmts,
+		}
+	}
 }
 
 func (c *simplifyContext) simplifyExpr(stmts *[]ast.Stmt, x ast.Expr) ast.Expr {
+	return c.simplifyExpr2(stmts, x, false)
+}
+
+func (c *simplifyContext) simplifyExpr2(stmts *[]ast.Stmt, x ast.Expr, callOK bool) ast.Expr {
 	switch x := x.(type) {
 	case *ast.FuncLit:
 		return &ast.FuncLit{
@@ -47,11 +338,11 @@ func (c *simplifyContext) simplifyExpr(stmts *[]ast.Stmt, x ast.Expr) ast.Expr {
 				elts[i] = &ast.KeyValueExpr{
 					Key:   kv.Key,
 					Colon: kv.Colon,
-					Value: c.newVar(stmts, kv.Value),
+					Value: c.simplifyExpr(stmts, kv.Value),
 				}
 				continue
 			}
-			elts[i] = c.newVar(stmts, elt)
+			elts[i] = c.simplifyExpr(stmts, elt)
 		}
 		return &ast.CompositeLit{
 			Type:   x.Type,
@@ -69,70 +360,59 @@ func (c *simplifyContext) simplifyExpr(stmts *[]ast.Stmt, x ast.Expr) ast.Expr {
 
 	case *ast.SelectorExpr:
 		return &ast.SelectorExpr{
-			X:   c.newVar(stmts, x.X),
+			X:   c.simplifyExpr(stmts, x.X),
 			Sel: x.Sel,
 		}
 
 	case *ast.IndexExpr:
 		return &ast.IndexExpr{
-			X:      c.newVar(stmts, x.X),
+			X:      c.simplifyExpr(stmts, x.X),
 			Lbrack: x.Lbrack,
-			Index:  c.newVar(stmts, x.Index),
+			Index:  c.simplifyExpr(stmts, x.Index),
 			Rbrack: x.Rbrack,
 		}
 
 	case *ast.SliceExpr:
 		return &ast.SliceExpr{
-			X:      c.newVar(stmts, x.X),
+			X:      c.simplifyExpr(stmts, x.X),
 			Lbrack: x.Lbrack,
-			Low:    c.newVar(stmts, x.Low),
-			High:   c.newVar(stmts, x.High),
-			Max:    c.newVar(stmts, x.Max),
+			Low:    c.simplifyExpr(stmts, x.Low),
+			High:   c.simplifyExpr(stmts, x.High),
+			Max:    c.simplifyExpr(stmts, x.Max),
 			Slice3: x.Slice3,
 			Rbrack: x.Rbrack,
 		}
 
 	case *ast.TypeAssertExpr:
 		return &ast.TypeAssertExpr{
-			X:      c.newVar(stmts, x.X),
+			X:      c.simplifyExpr(stmts, x.X),
 			Lparen: x.Lparen,
 			Type:   x.Type,
 			Rparen: x.Rparen,
 		}
 
 	case *ast.CallExpr:
-		fun := x.Fun
-		if _, ok := fun.(*ast.SelectorExpr); !ok {
-			fun = c.newVar(stmts, fun)
+		call := c.simplifyCall(stmts, x)
+		if callOK {
+			return call
 		}
-		args := make([]ast.Expr, len(x.Args))
-		for i, arg := range x.Args {
-			args[i] = c.newVar(stmts, arg)
-		}
-		return &ast.CallExpr{
-			Fun:      fun,
-			Lparen:   x.Lparen,
-			Args:     args,
-			Ellipsis: x.Ellipsis,
-			Rparen:   x.Rparen,
-		}
+		return c.newVar(stmts, call)
 
 	case *ast.StarExpr:
 		return &ast.StarExpr{
 			Star: x.Star,
-			X:    c.newVar(stmts, x.X),
+			X:    c.simplifyExpr(stmts, x.X),
 		}
 
 	case *ast.UnaryExpr:
 		return &ast.UnaryExpr{
 			OpPos: x.OpPos,
 			Op:    x.Op,
-			X:     c.newVar(stmts, x.X),
+			X:     c.simplifyExpr(stmts, x.X),
 		}
 
 	case *ast.BinaryExpr:
-		switch x.Op {
-		case token.LAND, token.LOR:
+		if (x.Op == token.LAND || x.Op == token.LOR) && ContainsCall(x.Y) {
 			v := c.newVar(stmts, x.X)
 			cond := v
 			if x.Op == token.LOR {
@@ -142,7 +422,7 @@ func (c *simplifyContext) simplifyExpr(stmts *[]ast.Stmt, x ast.Expr) ast.Expr {
 				}
 			}
 			var ifBody []ast.Stmt
-			ifBody = append(ifBody, simpleAssign(v, token.ASSIGN, c.simplifyExpr(&ifBody, x.Y)))
+			ifBody = append(ifBody, simpleAssign(v, token.ASSIGN, c.simplifyExpr2(&ifBody, x.Y, true)))
 			*stmts = append(*stmts, &ast.IfStmt{
 				Cond: cond,
 				Body: &ast.BlockStmt{
@@ -150,13 +430,12 @@ func (c *simplifyContext) simplifyExpr(stmts *[]ast.Stmt, x ast.Expr) ast.Expr {
 				},
 			})
 			return v
-		default:
-			return &ast.BinaryExpr{
-				X:     c.newVar(stmts, x.X),
-				OpPos: x.OpPos,
-				Op:    x.Op,
-				Y:     c.newVar(stmts, x.Y),
-			}
+		}
+		return &ast.BinaryExpr{
+			X:     c.simplifyExpr(stmts, x.X),
+			OpPos: x.OpPos,
+			Op:    x.Op,
+			Y:     c.simplifyExpr(stmts, x.Y),
 		}
 
 	default:
@@ -164,17 +443,44 @@ func (c *simplifyContext) simplifyExpr(stmts *[]ast.Stmt, x ast.Expr) ast.Expr {
 	}
 }
 
-func (c *simplifyContext) newVar(stmts *[]ast.Stmt, x ast.Expr) ast.Expr {
-	if x == nil {
-		return nil
+func (c *simplifyContext) simplifyCall(stmts *[]ast.Stmt, x *ast.CallExpr) *ast.CallExpr {
+	return &ast.CallExpr{
+		Fun:      c.simplifyExpr(stmts, x.Fun),
+		Lparen:   x.Lparen,
+		Args:     c.simplifyArgs(stmts, x.Args),
+		Ellipsis: x.Ellipsis,
+		Rparen:   x.Rparen,
 	}
-	if id, ok := x.(*ast.Ident); ok {
-		return id
-	}
+}
 
+func (c *simplifyContext) simplifyArgs(stmts *[]ast.Stmt, args []ast.Expr) []ast.Expr {
+	if len(args) == 1 {
+		if tuple, ok := c.info.TypeOf(args[0]).(*types.Tuple); ok {
+			call := c.simplifyExpr2(stmts, args[0], true)
+			vars := make([]ast.Expr, tuple.Len())
+			for i := range vars {
+				c.varCounter++
+				vars[i] = ast.NewIdent(fmt.Sprintf("_%d", c.varCounter))
+			}
+			*stmts = append(*stmts, &ast.AssignStmt{
+				Lhs: vars,
+				Tok: token.DEFINE,
+				Rhs: []ast.Expr{call},
+			})
+			return vars
+		}
+	}
+	simplifiedArgs := make([]ast.Expr, len(args))
+	for i, arg := range args {
+		simplifiedArgs[i] = c.simplifyExpr(stmts, arg)
+	}
+	return simplifiedArgs
+}
+
+func (c *simplifyContext) newVar(stmts *[]ast.Stmt, x ast.Expr) ast.Expr {
 	c.varCounter++
 	id := ast.NewIdent(fmt.Sprintf("_%d", c.varCounter))
-	*stmts = append(*stmts, simpleAssign(id, token.DEFINE, c.simplifyExpr(stmts, x)))
+	*stmts = append(*stmts, simpleAssign(id, token.DEFINE, x))
 	return id
 }
 
@@ -183,5 +489,39 @@ func simpleAssign(lhs ast.Expr, tok token.Token, rhs ast.Expr) *ast.AssignStmt {
 		Lhs: []ast.Expr{lhs},
 		Tok: tok,
 		Rhs: []ast.Expr{rhs},
+	}
+}
+
+func ContainsCall(x ast.Expr) bool {
+	switch x := x.(type) {
+	case *ast.CallExpr:
+		return true
+	case *ast.CompositeLit:
+		for _, elt := range x.Elts {
+			if ContainsCall(elt) {
+				return true
+			}
+		}
+		return false
+	case *ast.KeyValueExpr:
+		return ContainsCall(x.Key) || ContainsCall(x.Value)
+	case *ast.ParenExpr:
+		return ContainsCall(x.X)
+	case *ast.SelectorExpr:
+		return ContainsCall(x.X)
+	case *ast.IndexExpr:
+		return ContainsCall(x.X) || ContainsCall(x.Index)
+	case *ast.SliceExpr:
+		return ContainsCall(x.X) || ContainsCall(x.Low) || ContainsCall(x.High) || ContainsCall(x.Max)
+	case *ast.TypeAssertExpr:
+		return ContainsCall(x.X)
+	case *ast.StarExpr:
+		return ContainsCall(x.X)
+	case *ast.UnaryExpr:
+		return ContainsCall(x.X)
+	case *ast.BinaryExpr:
+		return ContainsCall(x.X) || ContainsCall(x.Y)
+	default:
+		return false
 	}
 }
--- simplify_test.go
+++ simplify_test.go
@@ -6,16 +6,13 @@ import (
 	"go/parser"
 	"go/printer"
 	"go/token"
+	"go/types"
 	"testing"
 )
 
 func TestSimplify(t *testing.T) {
-	simplifyAndCompare(t, "-a", "-a")
 	simplifyAndCompare(t, "-a()", "_1 := a(); -_1")
 	simplifyAndCompare(t, "a() + b()", "_1 := a(); _2 := b(); _1 + _2")
-	simplifyAndCompare(t, "a() && b()", "_1 := a(); if(_1) { _1 = b() }; _1")
-	simplifyAndCompare(t, "a() || b()", "_1 := a(); if(!_1) { _1 = b() }; _1")
-	simplifyAndCompare(t, "a() && (b() || c)", "_1 := a(); if(_1) { _2 := b(); if(!_2) { _2 = c }; _1 = (_2) }; _1")
 	simplifyAndCompare(t, "f(g(), h())", "_1 := g(); _2 := h(); f(_1, _2)")
 	simplifyAndCompare(t, "f().x", "_1 := f(); _1.x")
 	simplifyAndCompare(t, "f()()", "_1 := f(); _1()")
@@ -28,20 +25,80 @@ func TestSimplify(t *testing.T) {
 	simplifyAndCompare(t, "func() { -a() }", "func() { _1 := a(); -_1 }")
 	simplifyAndCompare(t, "T{a(), b()}", "_1 := a(); _2 := b(); T{_1, _2}")
 	simplifyAndCompare(t, "T{A: a(), B: b()}", "_1 := a(); _2 := b(); T{A: _1, B: _2}")
+
+	simplifyAndCompare(t, "a() && b", "_1 := a(); _1 && b")
+	simplifyAndCompare(t, "a && b()", "_1 := a; if _1 { _1 = b() }; _1")
+	simplifyAndCompare(t, "a() && b()", "_1 := a(); if _1 { _1 = b() }; _1")
+
+	simplifyAndCompare(t, "a() || b", "_1 := a(); _1 || b")
+	simplifyAndCompare(t, "a || b()", "_1 := a; if !_1 { _1 = b() }; _1")
+	simplifyAndCompare(t, "a() || b()", "_1 := a(); if !_1 { _1 = b() }; _1")
+
+	simplifyAndCompare(t, "a && (b || c())", "_1 := a; if(_1) { _2 := b; if(!_2) { _2 = c() }; _1 = (_2) }; _1")
+
+	simplifyAndCompare(t, "if a() { b }", "_1 := a(); if _1 { b }")
+	simplifyAndCompare(t, "if a := b(); a { c }", "{ a := b(); if a { c } }")
+	simplifyAndCompare(t, "if a { b()() }", "if a { _1 := b(); _1() }")
+	simplifyAndCompare(t, "if a { b } else { c()() }", "if a { b } else { _1 := c(); _1() }")
+	simplifyAndCompare(t, "if a { b } else if c { d()() }", "if a { b } else if c { _1 := d(); _1() }")
+	simplifyAndCompare(t, "if a { b } else if c() { d }", "if a { b } else { _1 := c(); if _1 { d } }")
+	simplifyAndCompare(t, "if a { b } else if c := d(); c { e }", "if a { b } else { c := d(); if c { e } }")
+
+	simplifyAndCompare(t, "switch a { case b, c: d()() }", "switch { default: _1 := a; if _1 == (b) || _1 == (c) { _2 := d(); _2() } }")
+	simplifyAndCompare(t, "switch a() { case b: c }", "switch { default: _1 := a(); if _1 == (b) { c } }")
+	simplifyAndCompare(t, "switch x := a(); x { case b, c: d }", "switch { default: x := a(); _1 := x; if _1 == (b) || _1 == (c) { d } }")
+	simplifyAndCompare(t, "switch a() { case b: c; default: e; case c: d }", "switch { default: _1 := a(); if _1 == (b) { c } else if _1 == (c) { d } else { e } }")
+	simplifyAndCompare(t, "switch a { case b(): c }", "switch { default: _1 := a; _2 := b(); if _1 == (_2) { c } }")
+	simplifyAndCompare(t, "switch a { default: d; fallthrough; case b: c }", "switch { default: _1 := a; if _1 == (b) { c } else { d; c } }")
+
+	simplifyAndCompare(t, "switch a().(type) { case b, c: d }", "_1 := a(); switch _1.(type) { case b, c: d }")
+	simplifyAndCompare(t, "switch x := a(); x.(type) { case b: c }", "{ x := a(); switch x.(type) { case b: c } }")
+	simplifyAndCompare(t, "switch a := b().(type) { case c: d }", "_1 := b(); switch a := _1.(type) { case c: d }")
+	simplifyAndCompare(t, "switch a.(type) { case b, c: d()() }", "switch a.(type) { case b, c: _1 := d(); _1() }")
+
+	simplifyAndCompare(t, "for a { b()() }", "for a { _1 := b(); _1() }")
+	// simplifyAndCompare(t, "for a() { b() }", "for { _1 := a(); if !_1 { break }; b() }")
+
+	// simplifyAndCompare(t, "select { case <-a(): b; case <-c(): d }", "_1 := a(); _2 := c(); select { case <-_1: b; case <-_2: d }")
+	simplifyAndCompare(t, "select { case <-a: b()() }", "select { case <-a: _1 := b(); _1() }")
+
+	simplifyAndCompare(t, "a().f++", "_1 := a(); _1.f++")
+	simplifyAndCompare(t, "go a()()", "_1 := a(); go _1()")
+	simplifyAndCompare(t, "defer a()()", "_1 := a(); defer _1()")
+	simplifyAndCompare(t, "a() <- b", "_1 := a(); _1 <- b")
+	simplifyAndCompare(t, "a <- b()", "_1 := b(); a <- _1")
+
+	simplifyAndCompare2(t, "f(g())", "_1, _2 := g(); f(_1, _2)", func(stmts []ast.Stmt) *types.Info {
+		g := stmts[0].(*ast.ExprStmt).X.(*ast.CallExpr).Args[0].(*ast.CallExpr)
+		return &types.Info{
+			Types: map[ast.Expr]types.TypeAndValue{
+				g: types.TypeAndValue{Type: types.NewTuple(
+					types.NewParam(0, nil, "x", nil),
+					types.NewParam(0, nil, "y", nil),
+				)},
+			},
+		}
+	})
 }
 
 func simplifyAndCompare(t *testing.T, in, out string) {
+	emptyTypes := func(stmts []ast.Stmt) *types.Info { return &types.Info{} }
+	simplifyAndCompare2(t, in, out, emptyTypes)
+	simplifyAndCompare2(t, out, out, emptyTypes)
+}
+
+func simplifyAndCompare2(t *testing.T, in, out string, mockTypes func([]ast.Stmt) *types.Info) {
 	fset := token.NewFileSet()
 
 	expected := fprint(t, fset, parse(t, fset, out))
 
 	file := parse(t, fset, in)
 	f := file.Decls[0].(*ast.FuncDecl)
-	f.Body.List = Simplify(f.Body.List)
+	f.Body.List = Simplify(f.Body.List, mockTypes(f.Body.List))
 	got := fprint(t, fset, file)
 
 	if got != expected {
-		t.Errorf("\n--- expected:\n%s--- got:\n%s", expected, got)
+		t.Errorf("\n--- input:\n%s\n--- expected output:\n%s--- got:\n%s", in, expected, got)
 	}
 }
 
@@ -59,4 +116,37 @@ func fprint(t *testing.T, fset *token.FileSet, file *ast.File) string {
 		t.Fatal(err)
 	}
 	return buf.String()
+}
+
+func TestContainsCall(t *testing.T) {
+	testContainsCall(t, "a", false)
+	testContainsCall(t, "a()", true)
+	testContainsCall(t, "T{a, b}", false)
+	testContainsCall(t, "T{a, b()}", true)
+	testContainsCall(t, "T{a: a, b: b()}", true)
+	testContainsCall(t, "(a())", true)
+	testContainsCall(t, "a().f", true)
+	testContainsCall(t, "a()[b]", true)
+	testContainsCall(t, "a[b()]", true)
+	testContainsCall(t, "a()[:]", true)
+	testContainsCall(t, "a[b():]", true)
+	testContainsCall(t, "a[:b()]", true)
+	testContainsCall(t, "a[:b:c()]", true)
+	testContainsCall(t, "a().(T)", true)
+	testContainsCall(t, "*a()", true)
+	testContainsCall(t, "-a()", true)
+	testContainsCall(t, "&a()", true)
+	testContainsCall(t, "&a()", true)
+	testContainsCall(t, "a() + b", true)
+	testContainsCall(t, "a + b()", true)
+}
+
+func testContainsCall(t *testing.T, in string, expected bool) {
+	x, err := parser.ParseExpr(in)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got := ContainsCall(x); got != expected {
+		t.Errorf("ContainsCall(%s): expected %t, got %t", in, expected, got)
+	}
 }
